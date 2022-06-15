fc978ef91bb79511b936cbe62f65fa684e4f85dc
u-aikay <uchey.ikenna@gmail.com>
2022-06-16 00:03:04 +0100

    register customer endpoint and test

--- src/test/java/com/eazybank/EazyBankApplicationTests.java
+++ src/test/java/com/eazybank/EazyBankApplicationTests.java
@@ -1,13 +1,7 @@
 package com.eazybank;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EazyBankApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
